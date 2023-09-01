bd04858dbf577003e20a7271d0ca6e2ef8e1136b
Cliff Chan <cliffchan1993@gmail.com>
2023-09-01 11:16:22 +0800

    Initial Commit

---
+++ testBottomSheetinSwiftUI/testBottomSheetinSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testBottomSheetinSwiftUIApp.swift
+//  testBottomSheetinSwiftUI
+//
+//  Created by Cliff Chan on 1/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct testBottomSheetinSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
